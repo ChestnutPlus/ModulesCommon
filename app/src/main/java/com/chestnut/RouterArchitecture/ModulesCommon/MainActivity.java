0d413af33c904266182e72bac58b2781fd76104f
Chestnut <974920378@qq.com>
2017-03-30 15:02:52 +0800

    2017年3月30日15:01:57  栗子 1 .增加了AudioRecordHelper未完成

--- app/src/main/java/com/chestnut/RouterArchitecture/ModulesCommon/MainActivity.java
+++ app/src/main/java/com/chestnut/RouterArchitecture/ModulesCommon/MainActivity.java
@@ -1,7 +1,11 @@
 package com.chestnut.RouterArchitecture.ModulesCommon;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.chesnut.Common.Helper.AudioUtil;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AudioUtil.getInstance().createFile();
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AudioUtil.getInstance().startRecord();
+                AudioUtil.getInstance().recordData();
+            }
+        });
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AudioUtil.getInstance().stopRecord();
+                AudioUtil.getInstance().convertWaveFile();
+            }
+        });
     }
 }
