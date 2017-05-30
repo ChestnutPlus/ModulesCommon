30a8124bcba3dc9c746374bf5430274a1a3acfd3
Chestnut <974920378@qq.com>
2017-05-30 17:09:03 +0800

    2017年5月30日17:08:24：宿舍 1. 完成：NotificationUtils 2. 删除旧版的：NotifiyUtil

---
+++ Common/src/main/java/com/chestnut/Common/ui/NotificationUtils.java
@@ -0,0 +1,88 @@
+package com.chestnut.Common.ui;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.support.annotation.DrawableRes;
+
+/**
+ * <pre>
+ *     author: Chestnut
+ *     blog  : http://www.jianshu.com/u/a0206b5f4526
+ *     time  : 2017/5/30 12:04
+ *     desc  :  封装系统的Notification
+ *     thanks To:
+ *          1.  http://www.jianshu.com/p/e1e20e0ee18c
+ *     dependent on:
+ *     update log:
+ * </pre>
+ */
+
+public class NotificationUtils {
+
+    /*  summary
+        设置系统默认提醒效果，一旦设置默认提醒效果，则自定义的提醒效果会全部失效。具体可看源码
+        <uses-permission android:name="android.permission.VIBRATE" />
+        Notification.DEFAULT_VIBRATE    //添加默认震动效果,需要申请震动权限
+        Notification.DEFAULT_SOUND      //添加系统默认声音效果，设置此值后，调用setSound()设置自定义声音无效
+        Notification.DEFAULT_LIGHTS     //添加默认呼吸灯效果，使用时须与 Notification.FLAG_SHOW_LIGHTS 结合使用，否则无效
+        Notification.DEFAULT_ALL        //添加上述三种默认提醒效果
+    */
+
+    /**
+     * 发送一个简单的默认通知：
+     * @param context   上下文
+     * @param ticker    摘要
+     * @param title     标题
+     * @param text      内容
+     * @param icon      icon
+     * @param msgId     消息ID
+     * @param pendingIntent 跳转Action
+     */
+    public static void sendDefault(Context context,
+                                   String ticker,
+                                   String title,
+                                   String text,
+                                   @DrawableRes int icon,
+                                   int msgId,
+                                   PendingIntent pendingIntent) {
+        send(context, ticker, title, text, icon, msgId, true, pendingIntent, Notification.DEFAULT_ALL);
+    }
+
+    /**
+     * 发送通知：
+     *      小图标，标题，内容
+     * @param context   上下文
+     * @param ticker    摘要
+     * @param title     标题
+     * @param text      内容
+     * @param icon      icon
+     * @param msgId     消息ID
+     * @param autoCancel 点击时取消
+     * @param pendingIntent 跳转Action
+     * @param defaults 额外的信息：振动/声音/灯光
+     */
+    public static void send(Context context,
+                            String ticker,
+                            String title,
+                            String text,
+                            @DrawableRes int icon,
+                            int msgId,
+                            boolean autoCancel,
+                            PendingIntent pendingIntent,
+                            int defaults) {
+        NotificationManager manager= (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification.Builder builder = new Notification.Builder(context);
+        builder.setTicker(ticker)
+                .setContentTitle(title)
+                .setContentText(text)
+                .setAutoCancel(autoCancel)
+                .setSmallIcon(icon)
+                .setWhen(System.currentTimeMillis())
+                .setContentIntent(pendingIntent)
+                .setDefaults(defaults)
+                .setPriority(Notification.PRIORITY_HIGH);
+        manager.notify(msgId,builder.build());
+    }
+}
--- app/src/main/java/com/chestnut/RouterArchitecture/ModulesCommon/MainActivity.java
+++ app/src/main/java/com/chestnut/RouterArchitecture/ModulesCommon/MainActivity.java
@@ -1,109 +1,35 @@
 package com.chestnut.RouterArchitecture.ModulesCommon;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.chestnut.Common.Helper.AudioRecordHelper;
-import com.chestnut.Common.Helper.MediaPlayerHelper;
-import com.chestnut.Common.Helper.MusicHelper;
-import com.chestnut.Common.utils.LogUtils;
+import com.chestnut.Common.ui.NotificationUtils;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private AudioRecordHelper audioRecordHelper = null;
-    private MusicHelper musicHelper = null;
-    private MediaPlayerHelper mediaPlayerHelper = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        audioRecordHelper = new AudioRecordHelper();
-        mediaPlayerHelper = new MediaPlayerHelper();
-        musicHelper = new MusicHelper();
-        audioRecordHelper.init(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)+"/Test.wav");
-        audioRecordHelper.setCallBack(new AudioRecordHelper.CallBack() {
-            @Override
-            public void onRecordStart(String file) {
-                LogUtils.w("onRecordStart",file);
-            }
-
-            @Override
-            public void onRecordDBChange(double dbValue) {
-                LogUtils.w("onRecordDBChange",dbValue+"");
-            }
-
-            @Override
-            public void onRecordFail(String file, String msg) {
-                LogUtils.w("onRecordFail",file+","+msg);
-            }
-
-            @Override
-            public void onRecordEnd(String file) {
-                LogUtils.w("onRecordEnd",file);
-            }
-        });
-
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                audioRecordHelper.startRecord();
-//                AudioUtil.getInstance().startRecord();
-//                AudioUtil.getInstance().recordData();
+                NotificationUtils.sendDefault(MainActivity.this,"ticker","title","text",R.mipmap.ic_launcher_round,12,null);
             }
         });
         findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                audioRecordHelper.stopRecord();
-//                AudioUtil.getInstance().stopRecord();
-//                AudioUtil.getInstance().convertWaveFile();
+
             }
         });
         findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                musicHelper.setUrl(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)+"/Test.wav").play();
-                startActivity(new Intent(MainActivity.this,ButtonsActivity.class));
-                mediaPlayerHelper.setUrl(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)+"/FenShou.mp3")
-                        .play(new MediaPlayerHelper.CallBack() {
-                            @Override
-                            public void onStart() {
-                                LogUtils.w("onStart");
-                            }
 
-                            @Override
-                            public void onRestart() {
-                                LogUtils.w("onRestart");
-                            }
-
-                            @Override
-                            public void onCompleted() {
-                                LogUtils.w("onCompleted");
-                            }
-
-                            @Override
-                            public void onStop() {
-                                LogUtils.w("onStop");
-                            }
-
-                            @Override
-                            public void onPause() {
-                                LogUtils.w("onPause");
-                            }
-
-                            @Override
-                            public void onError() {
-                                LogUtils.w("onError");
-                            }
-                        });
-
-//                musicHelper.setUrl(Environment.getExternalStorageDirectory().getAbsolutePath()+"/yinfu/yinfu.wav").play();
             }
         });
     }
